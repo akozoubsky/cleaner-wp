95bc8f12d8b16810bde0733b090edc1693665b53
Alexandre Kozoubsky <alexandre@alexandrekozoubsky.com>
2014-11-18 02:48:37 -0200

    cleaner-wp.js

--- js/cleaner-wp.js
+++ js/cleaner-wp.js
@@ -1,14 +1,5 @@
-// Targeting empty paragraphs.
-$('#content p')
-    .each(function() {
-    var $this = $(this);
-    if($this.html()
-    .replace(/\s| /g, '').length == 0)
-    $this.addClass('empty');
-});
-
 /* Open "post format link" links in a new tab */  
 jQuery(document).ready(function($) {
-jQuery('.format-link .entry-content a').attr('target','_blank');
+jQuery('.format-link a').attr('target','_blank');
 });
 
