5de11df0b3609738f78be230a53f9cb6056c9135
Alexandre Kozoubsky <akozoubsky@gmail.com>
2015-06-14 17:11:44 -0300

    Remove javascript for format-link

--- js/cleaner-wp.js
+++ js/cleaner-wp.js
@@ -1,6 +1,2 @@
-/* Open "post format link" links in a new tab */
-jQuery(document).ready(function($) {
-	jQuery('.format-link .entry-title a').attr('target','_blank');
-	jQuery('.format-link .entry-content a').attr('target','_blank');
-});
+
 
